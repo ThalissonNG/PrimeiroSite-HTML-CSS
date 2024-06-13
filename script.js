b12b86d32c05b5e616ce53bd9c8f0a48ad341014
ThalissonM <manoelthalisson28@gmail.com>
2024-06-13 16:39:16 -0300

    Adicionado script de javascript, e alterada a imagem do avatar no light mode

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function toggleMode(){
 const html = document.documentElement
+html.classList.toggle('light')
 
-if(html.classList.contains('light'))
+const img = document.querySelector("#profile img")
+
+if(html.classList.contains('light')){
+    img.setAttribute('src', './asets/avatar-light.png')
+    html.classList.remove('light')
+}else{
+    img.setAttribute('src', './asets/avatar.png')
+    html.classList.add('light')
+}
 }
